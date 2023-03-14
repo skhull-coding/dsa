407152f4077792ca8e12d910f748428dcff03e84
skhull <112753701+skhull-coding@users.noreply.github.com>
2023-03-14 12:31:46 +0000

    learn operations in arrays

---
+++ 4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Operations on arrays
+// Traversal - visiting every element of the array at least once
+// Insertion - Inserting a value at a given index in the array
+// Deletion - Deleting a value at a given index in the array
+// Searching - Searching a value in the array and giving its index
+//      if not sorted - linear search
+//      if sorted - binary search
+
+int main()
+{
+
+    return 0;
+}
