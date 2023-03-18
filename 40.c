8126e3cd17cb5526e4ce78a25c703e52c9ce1542
skhull <112753701+skhull-coding@users.noreply.github.com>
2023-03-18 05:51:05 +0000

    tree start

---
+++ 40.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Trees
+
+// Terminlologies ->
+// root - Topmost node
+// parent - Node which connects to the child
+// child - Node which is connected by another node is its child
+// sibling - Nodes belonging to the same parent
+// leaf/external - Nodes with no children
+// internal - Nodes with atleast one child
+// depth - No. of edges from root to that node
+// height - No. of edges from that node to the deepest leaf
+// degree - No. of direct children
+// tree degree - highest degree of a node among all the nodes present in the tree
+// Ancestors/descendants
+
+// Binary Tree ->
+// Tree which has atmost 2 children for all nodes
+// Tree of degree <= 2 (Nodes can have 0,1,2 children)
+
+// Tree - Non linear Data structure
+// Array, stack, linked list, queue - Linear Data Structure
+
+// Tree is ideal for representing hierarchial data
+// ex. Filesystem, etc.
+
+// Types of binary tree ->
+// 1. Full or strict binary tree - All nodes have either 0 or 2 children
+// 2. Perfect binary tree - Internal nodes have exactly 2 children + all leaf nodes are on the same level
+// 3. Complete binary tree - All levels are completely filled except possibly the last level + last level must have its keys as left as possible
+// 4. Degenerate tree - Every parent node has exactly one child
+// 5. Left/Right Skewed Degenerate tree - all children are on the left/right side of the tree
+
+
+int main()
+{
+    
+    
+    return 0;
+}
